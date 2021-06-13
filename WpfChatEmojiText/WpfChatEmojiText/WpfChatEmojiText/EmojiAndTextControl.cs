9da75c77cbd33f9cd860f893ada620afeba67e8f
驚鏵 闫 <yanjinhuamail@gmail.com>
2021-06-13 18:56:56 +0800

    updater.

---
+++ WpfChatEmojiText/WpfChatEmojiText/WpfChatEmojiText/EmojiAndTextControl.cs
@@ -0,0 +1,156 @@
+﻿/**
+ Github https://github.com/yanjinhuagood/WPFDevelopers.git
+ 码云 https://gitee.com/yanjinhua/WPFDevelopers.git
+ **/
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfChatEmojiText
+{
+    [TemplatePart(Name = TextBlockTemplateName, Type = typeof(TextBlock))]
+    [TemplatePart(Name = WrapPanelLeftTemplateName, Type = typeof(WrapPanel))]
+    [TemplatePart(Name = BorderTemplateName, Type = typeof(Border))]
+    [TemplatePart(Name = WrapPanelRightTemplateName, Type = typeof(WrapPanel))]
+    [TemplatePart(Name = ImageBrushLeftTemplateName, Type = typeof(ImageBrush))]
+    [TemplatePart(Name = ImageBrushRightTemplateName, Type = typeof(ImageBrush))]
+    public class EmojiAndTextControl: Control
+    {
+        private static readonly Type _typeofSelf = typeof(EmojiAndTextControl);
+        private const string TextBlockTemplateName = "PART_TextBlock";
+        private const string WrapPanelLeftTemplateName = "PART_Left";
+        private const string BorderTemplateName = "PART_Border";
+        private const string WrapPanelRightTemplateName = "PART_Right";
+        private const string ImageBrushLeftTemplateName = "LeftUser";
+        private const string ImageBrushRightTemplateName = "RightUser";
+        private TextBlock m_textBlock;
+        private WrapPanel leftWrapPanel;
+        private Border border;
+        private WrapPanel rightWrapPanel;
+        private ImageBrush leftImageBrush;
+        private ImageBrush rightImageBrush;
+        private bool m_IgnoreChanges = false;
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        static EmojiAndTextControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(_typeofSelf, new FrameworkPropertyMetadata(_typeofSelf));
+        }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            m_textBlock = GetTemplateChild(TextBlockTemplateName) as TextBlock;
+            leftWrapPanel = GetTemplateChild(WrapPanelLeftTemplateName) as WrapPanel;
+            border = GetTemplateChild(BorderTemplateName) as Border;
+            rightWrapPanel = GetTemplateChild(WrapPanelRightTemplateName) as WrapPanel;
+            leftImageBrush = GetTemplateChild(ImageBrushLeftTemplateName) as ImageBrush;
+            rightImageBrush = GetTemplateChild(ImageBrushRightTemplateName) as ImageBrush;
+            UpdateEmoji();
+            UpdateIsRight();
+            UpdateRightImageSource();
+            UpdateLeftImageSource();
+        }
+
+        public static readonly DependencyProperty TextProperty =
+          DependencyProperty.Register("Text", typeof(string), typeof(EmojiAndTextControl),
+            new UIPropertyMetadata(string.Empty, OnTextChanged));
+
+        static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            EmojiAndTextControl emoji = (EmojiAndTextControl)d;
+            emoji.UpdateEmoji();
+        }
+
+        void UpdateEmoji()
+        {
+            if (m_textBlock == null) return;
+            if (!m_IgnoreChanges)
+            {
+                m_IgnoreChanges = true;
+                //EmojiHelper.ParseText(m_textBlock);
+                EmojiHelperLibrary.EmojiHelper.Instance.ParseText(m_textBlock);
+                m_IgnoreChanges = false;
+            }
+        }
+
+
+
+        
+
+        public bool IsRight
+        {
+            get { return (bool)GetValue(IsRightProperty); }
+            set { SetValue(IsRightProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsRightProperty =
+            DependencyProperty.Register("IsRight", typeof(bool), typeof(EmojiAndTextControl), new UIPropertyMetadata(OnIsRightChanged));
+
+        static void OnIsRightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            EmojiAndTextControl control = (EmojiAndTextControl)d;
+            control.UpdateIsRight();
+        }
+
+        void UpdateIsRight()
+        {
+            if (leftWrapPanel == null
+                ||
+                border == null
+                ||
+                rightWrapPanel == null) return;
+            if (!IsRight)
+            {
+                leftWrapPanel.Visibility = Visibility.Visible;
+                border.Background = Brushes.White;
+                rightWrapPanel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        public ImageSource LeftImageSource
+        {
+            get { return (ImageSource)GetValue(LeftImageSourceProperty); }
+            set { SetValue(LeftImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty LeftImageSourceProperty =
+            DependencyProperty.Register("LeftImageSource", typeof(ImageSource), typeof(EmojiAndTextControl), new UIPropertyMetadata(OnLeftImageSourceChanged));
+        private static void OnLeftImageSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            EmojiAndTextControl control = (EmojiAndTextControl)d;
+            control.UpdateLeftImageSource();
+        }
+
+        void UpdateLeftImageSource()
+        {
+            if (leftImageBrush == null) return;
+            leftImageBrush.ImageSource = LeftImageSource;
+        }
+        public ImageSource RightImageSource
+        {
+            get { return (ImageSource)GetValue(RightImageSourceProperty); }
+            set { SetValue(RightImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty RightImageSourceProperty =
+            DependencyProperty.Register("RightImageSource", typeof(ImageSource), typeof(EmojiAndTextControl), new UIPropertyMetadata(OnRightImageSourceChanged));
+
+        private static void OnRightImageSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            EmojiAndTextControl control = (EmojiAndTextControl)d;
+            control.UpdateRightImageSource();
+        }
+       
+        void UpdateRightImageSource()
+        {
+            if (rightImageBrush == null) return;
+            rightImageBrush.ImageSource = RightImageSource;
+        }
+
+    }
+}
---
+++ WpfChatEmojiText/WpfChatEmojiText/WpfChatEmojiText/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace WpfChatEmojiText
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IEnumerable EmojiArray
+        {
+            get { return (IEnumerable)GetValue(EmojiArrayProperty); }
+            set { SetValue(EmojiArrayProperty, value); }
+        }
+
+        public static readonly DependencyProperty EmojiArrayProperty =
+            DependencyProperty.Register("EmojiArray", typeof(IEnumerable), typeof(MainWindow), new PropertyMetadata(null));
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var emojiModels = new List<EmojiModel>();
+
+
+            EmojiHelperLibrary.EmojiHelper.Instance._emojiHeight = 30;
+            EmojiHelperLibrary.EmojiHelper.Instance._emojiWidth = 30;
+
+            var m_Emojis = new Dictionary<string, string>();
+            var emojiPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "emoji");
+            var directory = new DirectoryInfo(emojiPath);
+            foreach (var item in directory.GetFiles())
+            {
+                var _key = $"[{Path.GetFileNameWithoutExtension(item.Name)}]";
+                m_Emojis.Add(_key, item.FullName);
+                emojiModels.Add(new EmojiModel { Name = Path.GetFileNameWithoutExtension(item.Name), Key = _key, Value = item.FullName });
+            }
+            EmojiHelperLibrary.EmojiHelper.Instance.m_Emojis = m_Emojis;
+
+            EmojiArray = emojiModels;
+
+        }
+
+        private void PART_Border_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            LeftButtonEmoji.IsChecked = false;
+            var send = sender as Border;
+            LeftInput.Text += send.Tag.ToString();
+            LeftInput.Focus();
+            LeftInput.SelectionStart = LeftInput.Text.Length;
+        }
+
+        private void LeftSend_Click(object sender, RoutedEventArgs e)
+        {
+            LeftChat();
+
+
+        }
+        void LeftChat()
+        {
+            var leftText = new EmojiAndTextControl();
+            leftText.IsRight = true;
+            leftText.RightImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/UserImages/jingtao.png"));
+            leftText.Text = LeftInput.Text;
+            var leftPara = new Paragraph();
+            leftPara.TextAlignment = TextAlignment.Right;
+            leftPara.Inlines.Add(leftText);
+            _LeftChat.Document.Blocks.Add(leftPara);
+
+
+            var rightText = new EmojiAndTextControl();
+            rightText.IsRight = false;
+            rightText.LeftImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/UserImages/jingtao.png"));
+            rightText.Text = LeftInput.Text;
+            var rightPara = new Paragraph();
+            rightPara.TextAlignment = TextAlignment.Left;
+            rightPara.Inlines.Add(rightText);
+            _RightChat.Document.Blocks.Add(rightPara);
+
+            LeftInput.Text = string.Empty;
+            LeftInput.Focus();
+        }
+        void RightChat()
+        {
+            var leftText = new EmojiAndTextControl();
+            leftText.IsRight = true;
+            leftText.RightImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/UserImages/yanjinhua.png"));
+            leftText.Text = RightInput.Text;
+            var leftPara = new Paragraph();
+            leftPara.TextAlignment = TextAlignment.Right;
+            leftPara.Inlines.Add(leftText);
+            _RightChat.Document.Blocks.Add(leftPara);
+
+
+            var rightText = new EmojiAndTextControl();
+            rightText.IsRight = false;
+            rightText.LeftImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/UserImages/yanjinhua.png"));
+            rightText.Text = RightInput.Text;
+            var rightPara = new Paragraph();
+            rightPara.TextAlignment = TextAlignment.Left;
+            rightPara.Inlines.Add(rightText);
+            _LeftChat.Document.Blocks.Add(rightPara);
+
+            RightInput.Text = string.Empty;
+            RightInput.Focus();
+        }
+
+        private void RightSend_Click(object sender, RoutedEventArgs e)
+        {
+            RightChat();
+        }
+
+        private void PART_Border_RightPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RightButtonEmoji.IsChecked = false;
+            var send = sender as Border;
+            RightInput.Text += send.Tag.ToString();
+            RightInput.Focus();
+            RightInput.SelectionStart = RightInput.Text.Length;
+        }
+    }
+    public class EmojiModel
+    {
+        public string Name { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
