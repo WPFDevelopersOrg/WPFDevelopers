463f2ea6178b49ea3bc7cd981c517ce7c847e538
驚鏵 闫 <yanjinhuamail@gmail.com>
2021-07-04 23:40:54 +0800

    updater...

---
+++ WpfDashboard/WpfDashboard/WpfDashboard/Models/ScaleModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Media;
+
+namespace WpfDashboard.Models
+{
+    public class ScaleModel : ViewModelBase
+    {
+        public int Index { get; set; }
+
+        private Brush _background;
+
+        public Brush Background
+        {
+            get { return _background; }
+            set
+            {
+                _background = value;
+                this.NotifyPropertyChange("Background");
+            }
+        }
+    }
+}
