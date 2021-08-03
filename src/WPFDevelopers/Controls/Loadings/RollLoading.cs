a2e678cb75df75f4f5ef90e9799ec526e87eed4c
吴锋 <wufeng4728704@hotmail.com>
2021-08-03 21:17:35 +0800

    新增 加载动画

---
+++ src/WPFDevelopers/Controls/Loadings/RollLoading.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPFDevelopers.Controls
+{
+    public class RollLoading : ContentControl
+    {
+        static RollLoading()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RollLoading), new FrameworkPropertyMetadata(typeof(RollLoading)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+
+
+
+
+        public Color ForegroundColor
+        {
+            get { return (Color)GetValue(ForegroundColorProperty); }
+            set { SetValue(ForegroundColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ForegroundColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ForegroundColorProperty =
+            DependencyProperty.Register("ForegroundColor", typeof(Color), typeof(RollLoading), new PropertyMetadata(Colors.Red));
+
+
+
+
+
+        public bool IsStart
+        {
+            get { return (bool)GetValue(IsStartProperty); }
+            set { SetValue(IsStartProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsStart.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsStartProperty =
+            DependencyProperty.Register("IsStart", typeof(bool), typeof(RollLoading), new PropertyMetadata(true));
+
+
+    }
+}
