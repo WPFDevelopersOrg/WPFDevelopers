9acb5dd6979949573366bd767ef2c94b64f98060
LuGuangGuang <272602298@qq.com>
2021-10-26 15:50:56 +0800

    修改RadarChartExmaple,数据的加载改为面向对象的方式，界面显示增加实际能力分数，增加显示具体对象的名称

--- src/WPFDevelopers.Samples/ExampleViews/RadarChartExample.xaml.cs
+++ src/WPFDevelopers.Samples/ExampleViews/RadarChartExample.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using WPFDevelopers.Controls;
@@ -16,42 +19,107 @@ namespace WPFDevelopers.Samples.ExampleViews
             get { return (ObservableCollection<RadarModel>)GetValue(RadarModelsProperty); }
             set { SetValue(RadarModelsProperty, value); }
         }
-
         public static readonly DependencyProperty RadarModelsProperty =
-            DependencyProperty.Register("RadarModels", typeof(ObservableCollection<RadarModel>), typeof(RadarChartExample), new PropertyMetadata(null));
+       DependencyProperty.Register("RadarModels", typeof(ObservableCollection<RadarModel>), typeof(RadarChartExample), new PropertyMetadata(null));
+        List<Player> Players = new List<Player>();
+        private int NowPlayerIndex = 0;
+        public string NowPlayerName
+        {
+            get { return (string)GetValue(NowPlayerNameProperty); }
+            set { SetValue(NowPlayerNameProperty, value); }
+        }
+        public static readonly DependencyProperty NowPlayerNameProperty =
+     DependencyProperty.Register("NowPlayerName", typeof(string), typeof(RadarChartExample), new PropertyMetadata(null));
+
         List<ObservableCollection<RadarModel>> collectionList = new List<ObservableCollection<RadarModel>>();
         public RadarChartExample()
         {
             InitializeComponent();
             RadarModels = new ObservableCollection<RadarModel>();
-            var collection1 = new ObservableCollection<RadarModel>();
-            collection1.Add(new RadarModel { Text = "击杀", ValueMax = 95});
-            collection1.Add(new RadarModel { Text = "生存", ValueMax = 80 });
-            collection1.Add(new RadarModel { Text = "助攻", ValueMax = 70 });
-            collection1.Add(new RadarModel { Text = "物理", ValueMax = 80 });
-            collection1.Add(new RadarModel { Text = "魔法", ValueMax = 90 });
-            collection1.Add(new RadarModel { Text = "防御", ValueMax = 87 });
-            collection1.Add(new RadarModel { Text = "金钱", ValueMax = 59 });
+            Player theShy = new Player()
+            {
+                姓名 = "The Shy",
+                击杀 = 80,
+                助攻 = 50,
+                物理 = 90,
+                生存 = 10,
+                金钱 = 60,
+                防御 = 30,
+                魔法 = 30
+            };
+            Player xiaoHu = new Player()
+            {
+                姓名 = "销户",
+                击杀 = 50,
+                助攻 = 50,
+                物理 = 50,
+                生存 = 50,
+                金钱 = 50,
+                防御 = 50,
+                魔法 = 50
+            };
+            Player yinHang = new Player()
+            {
+                姓名 = "狼行",
+                击杀 = 40,
+                助攻 = 60,
+                物理 = 60,
+                生存 = 90,
+                金钱 = 40,
+                防御 = 80,
+                魔法 = 60
+            };
+            Player flandre = new Player()
+            {
+                姓名 = "圣枪哥",
+                击杀 = 60,
+                助攻 = 70,
+                物理 = 80,
+                生存 = 70,
+                金钱 = 80,
+                防御 = 80,
+                魔法 = 30
+            };
+            Players.AddRange(new[] { theShy, xiaoHu, yinHang, flandre });
 
-            var collection2 = new ObservableCollection<RadarModel>();
-            collection2.Add(new RadarModel { Text = "击杀", ValueMax = 59 });
-            collection2.Add(new RadarModel { Text = "生存", ValueMax = 80 });
-            collection2.Add(new RadarModel { Text = "助攻", ValueMax = 90 });
-            collection2.Add(new RadarModel { Text = "物理", ValueMax = 70 });
-            collection2.Add(new RadarModel { Text = "魔法", ValueMax = 80 });
-            collection2.Add(new RadarModel { Text = "防御", ValueMax = 90 });
-            collection2.Add(new RadarModel { Text = "金钱", ValueMax = 66 });
-            collectionList.AddRange(new[] { collection1, collection2 });
+            Type t = theShy.GetType();
+            PropertyInfo[] pArray = t.GetProperties();
+            pArray = pArray.Where(it => it.PropertyType == typeof(int)).ToArray();
+
+            foreach (var player in Players)
+            {
+                var collectionpPayer = new ObservableCollection<RadarModel>();
+                Array.ForEach<PropertyInfo>(pArray, p =>
+                {
+                    collectionpPayer.Add(new RadarModel { Text = $"{p.Name}（{(int)p.GetValue(player, null)}分）", ValueMax = (int)p.GetValue(player, null) });
+                });
+                collectionList.Add(collectionpPayer);
+            }
             RadarModels = collectionList[0];
+            NowPlayerName = Players[0].姓名;
         }
-        bool isRefresh = false;
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (!isRefresh)
-                RadarModels = collectionList[1];
-            else
-                RadarModels = collectionList[0];
-            isRefresh = !isRefresh;
+            NowPlayerIndex++;
+            if (NowPlayerIndex >= collectionList.Count)
+            {
+                NowPlayerIndex = 0;
+            }
+            RadarModels = collectionList[NowPlayerIndex];
+            NowPlayerName = Players[NowPlayerIndex].姓名;
         }
+    }
+
+    public class Player
+    {
+        public string 姓名 { get; set; }
+        public int 击杀 { get; set; }
+        public int 生存 { get; set; }
+        public int 助攻 { get; set; }
+        public int 物理 { get; set; }
+        public int 魔法 { get; set; }
+        public int 防御 { get; set; }
+        public int 金钱 { get; set; }
     }
 }
