eb93c6424f63ded93139b3fb77e93ec5f3d633dc
闫驚鏵（Jinhua  Yan） <yanjinhuamail@gmail.com>
2022-12-05 18:42:16 +0800

    add in GrayscaleEffect

--- src/WPFDevelopers.Samples/ExampleViews/MainWindow.xaml.cs
+++ src/WPFDevelopers.Samples/ExampleViews/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Interop;
+using System.Windows.Media.Animation;
 using WPFDevelopers.Controls;
 using WPFDevelopers.Samples.ExampleViews;
 using MessageBox = WPFDevelopers.Controls.MessageBox;
@@ -43,6 +44,32 @@ namespace WPFDevelopers.Samples
         {
             WpfNotifyIcon.IsTwink = !WpfNotifyIcon.IsTwink;
             menuItemTwink.IsChecked = WpfNotifyIcon.IsTwink;
+        }
+        private void Grayscale_Click(object sender, RoutedEventArgs e)
+        {
+            if(grayscaleEffect.Factor == 1)
+            {
+                menuItemGrayscale.Header = "关闭灰度";
+                Create(0);
+            }
+               
+            else
+            {
+                menuItemGrayscale.Header = "开启灰度";
+                Create(1);
+
+            }
+        }
+        void Create(double to)
+        {
+            var sineEase = new SineEase() { EasingMode = EasingMode.EaseOut };
+            var doubleAnimation = new DoubleAnimation
+            {
+                To = to,
+                Duration = TimeSpan.FromMilliseconds(1000),
+                EasingFunction = sineEase
+            };
+            grayscaleEffect.BeginAnimation(GrayscaleEffect.FactorProperty, doubleAnimation);
         }
     }
 }
---
+++ src/WPFDevelopers.Shared/Effects/GrayscaleEffect.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace WPFDevelopers
+{
+    public class GrayscaleEffect : ShaderEffect
+    {
+        /// <summary>
+        /// Identifies the Input property.
+        /// </summary>
+        public static readonly DependencyProperty InputProperty = RegisterPixelShaderSamplerProperty("Input", typeof(GrayscaleEffect), 0);
+
+        /// <summary>
+        /// Identifies the Factor property.
+        /// </summary>
+        public static readonly DependencyProperty FactorProperty = DependencyProperty.Register("Factor", typeof(double), typeof(GrayscaleEffect), new UIPropertyMetadata(0D, PixelShaderConstantCallback(0)));
+
+        /// <summary>
+        /// Identifies the Brightness property.
+        /// </summary>
+        public static readonly DependencyProperty BrightnessProperty = DependencyProperty.Register("Brightness", typeof(double), typeof(GrayscaleEffect), new UIPropertyMetadata(0D, PixelShaderConstantCallback(1)));
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="GrayscaleEffect"/> class.
+        /// </summary>
+        public GrayscaleEffect()
+        {
+            var pixelShader = new PixelShader();
+            pixelShader.UriSource = new Uri("WPFDevelopers;component/Effects/GrayscaleEffect.ps", UriKind.Relative);
+
+            PixelShader = pixelShader;
+
+            UpdateShaderValue(InputProperty);
+            UpdateShaderValue(FactorProperty);
+            UpdateShaderValue(BrightnessProperty);
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Brush"/> used as input for the shader.
+        /// </summary>
+        public Brush Input
+        {
+            get => ((Brush)(GetValue(InputProperty)));
+            set => SetValue(InputProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the factor used in the shader.
+        /// </summary>
+        public double Factor
+        {
+            get => ((double)(GetValue(FactorProperty)));
+            set => SetValue(FactorProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the brightness of the effect.
+        /// </summary>
+        public double Brightness
+        {
+            get => ((double)(GetValue(BrightnessProperty)));
+            set => SetValue(BrightnessProperty, value);
+        }
+    }
+}
