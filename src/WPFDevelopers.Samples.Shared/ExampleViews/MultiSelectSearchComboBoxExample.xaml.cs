7f01d105be842e384fc3c94338ca5fe133e92e9f
闫驚鏵（Jinhua  Yan） <yanjinhuamail@gmail.com>
2023-04-16 22:45:13 +0800

    add in MultiSelectionSearchComboBox

---
+++ src/WPFDevelopers.Samples.Shared/ExampleViews/MultiSelectSearchComboBoxExample.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFDevelopers.Samples.ExampleViews
+{
+    /// <summary>
+    /// MultiSelectComboBoxExample.xaml 的交互逻辑
+    /// </summary>
+    public partial class MultiSelectSearchComboBoxExample : UserControl
+    {
+        public MultiSelectSearchComboBoxExample()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            WPFDevelopers.Controls.MessageBox.Show($"{MyMultiSelectionSearchComboBox2.Text} \r\n总共选中:{MyMultiSelectionSearchComboBox2.SelectedItems.Count} 条","选中内容",MessageBoxButton.OK,MessageBoxImage.Information);      
+        }
+    }
+}
