d33809082c6aebd4bf28790873f90dd95af645f9
闫驚鏵（Jinhua  Yan） <yanjinhuamail@gmail.com>
2023-05-14 22:00:07 +0800

    Create StepIndexConverter.cs

---
+++ src/WPFDevelopers.Shared/Converts/StepIndexConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WPFDevelopers.Controls;
+
+namespace WPFDevelopers.Converts
+{
+
+    [ValueConversion(typeof(Int32), typeof(StepItem))]
+    public class StepIndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type TargetType, object parameter, CultureInfo culture)
+        {
+            var item = (StepItem)value;
+            var step = ItemsControl.ItemsControlFromItemContainer(item) as Step;
+            var index = step.ItemContainerGenerator.IndexFromContainer(item) + 1;
+            item.Index = index;
+            return index;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
