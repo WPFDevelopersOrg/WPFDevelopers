044625a32f10309a83e4c3046a20eb8c43528a7c
闫驚鏵 <yanjinhuamail@163.com>
2023-07-24 23:57:48 +0800

    Create HalfValueConverter.cs

---
+++ src/WPFDevelopers.Shared/Converts/HalfValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFDevelopers.Converts
+{
+    public class HalfValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double actualValue)
+                return actualValue / 2;
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
