c834db0272064057aaa6e9055a4cb900c5a3a9c8
闫驚鏵 <yanjinhuamail@163.com>
2023-07-31 23:46:56 +0800

    add in NumericBoxExample
    Co-Authored-By: huangjia2107 <654664168@qq.com>

---
+++ src/WPFDevelopers.Samples.Shared/ExampleViews/NumericBoxExample.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WPFDevelopers.Samples.ExampleViews
+{
+    /// <summary>
+    /// MessageExample.xaml 的交互逻辑
+    /// </summary>
+    public partial class NumericBoxExample : UserControl
+    {
+        public NumericBoxExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
