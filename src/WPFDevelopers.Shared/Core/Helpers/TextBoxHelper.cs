c3909f7f967af6c106110f83c686a0ce43f55865
闫驚鏵 <yanjinhuamail@163.com>
2023-08-08 23:21:42 +0800

    Update TextBoxHelper.cs

--- src/WPFDevelopers.Shared/Core/Helpers/TextBoxHelper.cs
+++ src/WPFDevelopers.Shared/Core/Helpers/TextBoxHelper.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Text.RegularExpressions;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -10,28 +8,34 @@ namespace WPFDevelopers.Helpers
     public static class TextBoxHelper
     {
         public static readonly DependencyProperty SelectAllOnClickProperty =
-            DependencyProperty.RegisterAttached("SelectAllOnClick", typeof(bool), typeof(TextBoxHelper), new PropertyMetadata(false, OnSelectAllOnClickChanged));
+            DependencyProperty.RegisterAttached("SelectAllOnClick", typeof(bool), typeof(TextBoxHelper),
+                new PropertyMetadata(false, OnSelectAllOnClickChanged));
+
+
+        public static readonly DependencyProperty AllowOnlyNumericInputProperty =
+            DependencyProperty.RegisterAttached("AllowOnlyNumericInput", typeof(bool), typeof(TextBoxHelper),
+                new PropertyMetadata(false, OnAllowOnlyNumericInputChanged));
+
+        public static readonly DependencyProperty IsEnterUpdateEnabledProperty =
+            DependencyProperty.RegisterAttached("IsEnterUpdateEnabled", typeof(bool), typeof(TextBoxHelper),
+                new PropertyMetadata(false, OnIsEnterUpdateEnabledChanged));
 
         private static void OnSelectAllOnClickChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is TextBox textBox)
             {
-                if ((bool)e.NewValue)
+                if ((bool) e.NewValue)
                     textBox.PreviewMouseLeftButtonDown += TextBox_PreviewMouseLeftButtonDown;
                 else
                     textBox.PreviewMouseLeftButtonDown -= TextBox_PreviewMouseLeftButtonDown;
             }
         }
 
-
-        public static readonly DependencyProperty AllowOnlyNumericInputProperty =
-       DependencyProperty.RegisterAttached("AllowOnlyNumericInput", typeof(bool), typeof(TextBoxHelper), new PropertyMetadata(false, OnAllowOnlyNumericInputChanged));
-
         private static void OnAllowOnlyNumericInputChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is TextBox textBox)
             {
-                if ((bool)e.NewValue)
+                if ((bool) e.NewValue)
                 {
                     DataObject.AddPastingHandler(textBox, TextBox_Pasting);
                     textBox.PreviewTextInput += TextBox_PreviewTextInput;
@@ -46,10 +50,20 @@ namespace WPFDevelopers.Helpers
             }
         }
 
+        private static void OnIsEnterUpdateEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox textBox)
+            {
+                if ((bool) e.NewValue)
+                    textBox.PreviewKeyDown += TextBox_PreviewKeyDown;
+                else
+                    textBox.PreviewKeyDown -= TextBox_PreviewKeyDown;
+            }
+        }
 
         public static bool GetSelectAllOnClick(TextBox textBox)
         {
-            return (bool)textBox.GetValue(SelectAllOnClickProperty);
+            return (bool) textBox.GetValue(SelectAllOnClickProperty);
         }
 
         public static void SetSelectAllOnClick(TextBox textBox, bool value)
@@ -59,12 +73,22 @@ namespace WPFDevelopers.Helpers
 
         public static bool GetAllowOnlyNumericInput(DependencyObject obj)
         {
-            return (bool)obj.GetValue(AllowOnlyNumericInputProperty);
+            return (bool) obj.GetValue(AllowOnlyNumericInputProperty);
         }
 
         public static void SetAllowOnlyNumericInput(DependencyObject obj, bool value)
         {
             obj.SetValue(AllowOnlyNumericInputProperty, value);
+        }
+
+        public static bool GetIsEnterUpdateEnabled(DependencyObject obj)
+        {
+            return (bool) obj.GetValue(IsEnterUpdateEnabledProperty);
+        }
+
+        public static void SetIsEnterUpdateEnabled(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsEnterUpdateEnabledProperty, value);
         }
 
         private static void TextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -82,12 +106,9 @@ namespace WPFDevelopers.Helpers
         {
             if (e.DataObject.GetDataPresent(typeof(string)))
             {
-                string text = (string)e.DataObject.GetData(typeof(string));
+                var text = (string) e.DataObject.GetData(typeof(string));
 
-                if (!IsNumeric(text))
-                {
-                    e.CancelCommand();
-                }
+                if (!IsNumeric(text)) e.CancelCommand();
             }
             else
             {
@@ -95,14 +116,10 @@ namespace WPFDevelopers.Helpers
             }
         }
 
-        
 
         private static void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            if (!IsNumeric(e.Text) || ContainsChineseCharacters(e.Text))
-            {
-                e.Handled = true;
-            }
+            if (!IsNumeric(e.Text) || ContainsChineseCharacters(e.Text)) e.Handled = true;
         }
 
         private static bool IsNumeric(string text)
@@ -112,15 +129,20 @@ namespace WPFDevelopers.Helpers
 
         private static bool ContainsChineseCharacters(string text)
         {
-            foreach (char c in text)
-            {
+            foreach (var c in text)
                 if (c >= 0x4E00 && c <= 0x9FFF)
-                {
                     return true;
-                }
-            }
             return false;
         }
 
+        private static void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && !e.IsRepeat)
+            {
+                var textBox = (TextBox) sender;
+                var expression = textBox.GetBindingExpression(TextBox.TextProperty);
+                expression?.UpdateSource();
+            }
+        }
     }
 }
