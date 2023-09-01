fa8ca2246193fd318d644417f74520c7dafcfcde
闫驚鏵 <yanjinhuamail@163.com>
2023-09-01 23:00:27 +0800

    Update AMapTitleLayer.cs
    add in style

--- src/WPFDevelopers.Samples.Shared/ExampleViews/Map/AMapTitleLayer.cs
+++ src/WPFDevelopers.Samples.Shared/ExampleViews/Map/AMapTitleLayer.cs
@@ -5,9 +5,11 @@ namespace WPFDevelopers.Samples.ExampleViews
 {
     public class AMapTitleLayer : MapTileLayer
     {
+        private AMapTileSource tileSource;
         public AMapTitleLayer()
         {
-            TileSource = new AMapTileSource();
+            tileSource = new AMapTileSource();
+            TileSource = tileSource;
         }
 
         public string UriFormat
@@ -15,13 +17,23 @@ namespace WPFDevelopers.Samples.ExampleViews
             get { return TileSource.UriFormat; }
             set { TileSource.UriFormat = value; }
         }
+        public void UpdateTileSourceStyle(int style)
+        {
+            tileSource.UpdateStyle(style);
+        }
     }
     public class AMapTileSource : TileSource
     {
+        private int style = 7;
         public override Uri GetUri(int x, int y, int zoomLevel)
         {
-            string url = string.Format("http://wprd01.is.autonavi.com/appmaptile?x={0}&y={1}&z={2}&lang=zh_cn&size=1&scl=1&style=7", x, y, zoomLevel);
+            string url = string.Format("http://wprd01.is.autonavi.com/appmaptile?x={0}&y={1}&z={2}&lang=zh_cn&size=1&scl=1&style={3}", x, y, zoomLevel, style);
             return new Uri(url, UriKind.Absolute);
+        }
+        public void UpdateStyle(int newStyle)
+        {
+            if (newStyle == style) return;
+            style = newStyle;
         }
     }
 }
