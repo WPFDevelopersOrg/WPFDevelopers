0e316e587f28f064cf96f833c1b6d188d35e55d4
闫驚鏵 <yanjinhuamail@163.com>
2024-01-24 23:35:10 +0800

    update in TransformLayout
    update in TransformLayout

--- src/WPFDevelopers.Samples.Shared/ExampleViews/TransformLayoutExample.xaml.cs
+++ src/WPFDevelopers.Samples.Shared/ExampleViews/TransformLayoutExample.xaml.cs
@@ -3,11 +3,11 @@
 namespace WPFDevelopers.Samples.ExampleViews
 {
     /// <summary>
-    /// TransformThumbExample.xaml 的交互逻辑
+    /// TransformLayoutExample.xaml 的交互逻辑
     /// </summary>
-    public partial class TransformThumbExample : UserControl
+    public partial class TransformLayoutExample : UserControl
     {
-        public TransformThumbExample()
+        public TransformLayoutExample()
         {
             InitializeComponent();
         }
