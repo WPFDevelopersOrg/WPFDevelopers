33534f2406a9e833ecca0353943c3b0d8ca120b6
闫驚鏵 <yanjinhuamail@163.com>
2024-02-19 18:26:11 +0800

    Create WaterfallPanel ( #70  )
     Create WaterfallPanel ( #70  )

---
+++ src/WPFDevelopers.Samples.Shared/ExampleViews/WaterfallPanelExample.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WPFDevelopers.Samples.ExampleViews
+{
+    /// <summary>
+    /// WaterfallPanelExample.xaml 的交互逻辑
+    /// </summary>
+    public partial class WaterfallPanelExample : UserControl
+    {
+        public WaterfallPanelExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
