344c170da05202b1ac55561cfd91b1d213efb34a
闫驚鏵 <yanjinhuamail@163.com>
2024-02-19 18:26:18 +0800

    Create OrientationConverter.cs

---
+++ src/WPFDevelopers.Shared/Core/Converts/OrientationConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WPFDevelopers.Converts
+{
+    public class OrientationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Orientation orientation)
+                return (orientation == Orientation.Horizontal) ? Orientation.Vertical : Orientation.Horizontal;
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
