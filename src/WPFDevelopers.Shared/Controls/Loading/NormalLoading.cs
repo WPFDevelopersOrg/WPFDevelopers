4ec993549bb009678140d8d3d26ea0d1382ee770
闫驚鏵 <yanjinhuamail@163.com>
2024-02-21 22:16:30 +0800

    Update NormalLoading.cs

--- src/WPFDevelopers.Shared/Controls/Loading/NormalLoading.cs
+++ src/WPFDevelopers.Shared/Controls/Loading/NormalLoading.cs
@@ -1,61 +1,32 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Expression.Drawing.Media;
-using Microsoft.Expression.Drawing.Shapes;
 
 namespace WPFDevelopers.Controls
 {
-    [TemplatePart(Name = IndicatorTemplateName, Type = typeof(Arc))]
-    public class NormalLoading : RangeBase
+    public class NormalLoading: Control
     {
-        private const string IndicatorTemplateName = "PART_Indicator";
+        public static readonly DependencyProperty StrokeArrayProperty =
+            DependencyProperty.Register("StrokeArray", typeof(DoubleCollection), typeof(NormalLoading)
+                , new PropertyMetadata(new DoubleCollection {20, 100}));
 
-        public static readonly DependencyProperty ArcThicknessProperty = 
-            DependencyProperty.Register("ArcThickness", typeof(double), typeof(NormalLoading),
-                new PropertyMetadata(0d));
+        //public static NormalLoading Default = new NormalLoading();
 
-        public static readonly DependencyProperty IsIndeterminateProperty =
-           ProgressBar.IsIndeterminateProperty.AddOwner(typeof(NormalLoading),
-               new FrameworkPropertyMetadata(true));
-
-        private Arc _indicator;
-
-        public double ArcThickness
+        static NormalLoading()
         {
-            get => (double)GetValue(ArcThicknessProperty);
-            set => SetValue(ArcThicknessProperty, value);
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NormalLoading),
+                new FrameworkPropertyMetadata(typeof(NormalLoading)));
         }
-        public bool IsIndeterminate
+
+        public DoubleCollection StrokeArray
         {
-            get => (bool)GetValue(IsIndeterminateProperty);
-            set => SetValue(IsIndeterminateProperty, value);
+            get => (DoubleCollection) GetValue(StrokeArrayProperty);
+            set => SetValue(StrokeArrayProperty, value);
         }
 
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            _indicator = GetTemplateChild(IndicatorTemplateName) as Arc;
-            if (_indicator != null)
-            {
-                _indicator.StartAngle = 0;
-                _indicator.EndAngle = 0;
-            }
-            SetProgressBarIndicatorAngle();
-        }
-
-        private void SetProgressBarIndicatorAngle()
-        {
-            if (_indicator == null) return;
-            var minimum = Minimum;
-            var maximum = Maximum;
-            var num = Value;
-            _indicator.EndAngle = (maximum <= minimum ? 0 : (num - minimum) / (maximum - minimum)) * 360;
         }
     }
-
 }
