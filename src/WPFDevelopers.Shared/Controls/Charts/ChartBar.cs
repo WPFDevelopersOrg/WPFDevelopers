d4a8872be29fc012eb20588174998a2ba5f22737
闫驚鏵 <yanjinhuamail@163.com>
2024-05-18 15:29:58 +0800

    Update ChartBar.cs

--- src/WPFDevelopers.Shared/Controls/Charts/ChartBar.cs
+++ src/WPFDevelopers.Shared/Controls/Charts/ChartBar.cs
@@ -9,6 +9,7 @@ namespace WPFDevelopers.Controls
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
+            var dicts = new Dictionary<Rect, string>();
             var x = StartX;
             var rectWidth = 85;
             var interval = Interval;
@@ -22,7 +23,10 @@ namespace WPFDevelopers.Controls
                 var rect = new Rect(x + (interval - rectWidth) / 2, StartY - rectHeight, rectWidth, rectHeight);
                 drawingContext.DrawRectangle(NormalBrush, null, rect);
                 x += interval;
+                var nRect = new Rect(rect.Left - EllipsePadding, rect.Top - EllipsePadding, rect.Width + EllipsePadding, rect.Height + EllipsePadding);
+                dicts.Add(nRect, $"{item.Key} : {item.Value}");
             }
+            PointCache = dicts;
         }
     }
 }
