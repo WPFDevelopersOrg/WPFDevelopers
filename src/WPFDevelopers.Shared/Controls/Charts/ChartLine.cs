3d2b69b8a1edb0cd3731fd3844137f12a13aa1ba
闫驚鏵 <yanjinhuamail@163.com>
2024-05-27 22:27:37 +0800

    Update ChartLine.cs

--- src/WPFDevelopers.Shared/Controls/Charts/ChartLine.cs
+++ src/WPFDevelopers.Shared/Controls/Charts/ChartLine.cs
@@ -6,7 +6,7 @@ using System.Windows.Media;
 
 namespace WPFDevelopers.Controls
 {
-    public class ChartLine : ChartBase
+    public class ChartLine : ChartRect
     {
         protected override void OnRender(DrawingContext drawingContext)
         {
@@ -14,11 +14,13 @@ namespace WPFDevelopers.Controls
                 return;
             base.OnRender(drawingContext);
             var dicts = new Dictionary<Rect, string>();
+            var rects = new List<Rect>();
+            var points = new List<Point>();
             var x = StartX;
             var interval = Interval;
             var drawingPen = new Pen
             {
-                Thickness = 1,
+                Thickness = 2,
                 Brush = NormalBrush
             };
             drawingPen.Freeze();
@@ -28,6 +30,7 @@ namespace WPFDevelopers.Controls
             double proportion = firstDataPoint.Value / IntervalY;
             double yPositionFromBottom = StartY - proportion * (ScaleFactor * Rows);
             var startPoint = new Point(x + Interval / 2, yPositionFromBottom);
+            points.Add(startPoint);
             foreach (var item in Datas)
             {
                 var formattedText = DrawingContextHelper.GetFormattedText(item.Key,
@@ -37,17 +40,38 @@ namespace WPFDevelopers.Controls
 
                 var y = StartY - (item.Value / IntervalY) * (ScaleFactor * Rows);
                 var endPoint = new Point(x + Interval / 2, y);
+                points.Add(endPoint);
                 drawingContext.DrawLine(drawingPen, startPoint, endPoint);
                 var ellipsePoint = new Point(endPoint.X - EllipseSize / 2, endPoint.Y - EllipseSize / 2);
                 var rect = new Rect(ellipsePoint, new Size(EllipseSize, EllipseSize));
-                var ellipseGeom = new EllipseGeometry(rect);
-                drawingContext.DrawGeometry(drawingPen.Brush, drawingPen, ellipseGeom);
+                rects.Add(rect);
                 startPoint = endPoint;
                 x += interval;
                 var nRect = new Rect(rect.Left - EllipsePadding, rect.Top - EllipsePadding, rect.Width + EllipsePadding, rect.Height + EllipsePadding);
                 dicts.Add(nRect, $"{item.Key} : {item.Value}");
             }
             PointCache = dicts;
+            if(points.Count > 1)
+            {
+                var color = (Color)Application.Current.TryFindResource("WD.PrimaryNormalColor");
+                var rectBrush = new SolidColorBrush(color);
+                rectBrush.Opacity = 0.3;
+                rectBrush.Freeze();
+                var streamGeometry = new StreamGeometry();
+                using (StreamGeometryContext geometryContext = streamGeometry.Open())
+                {
+                    var sPoint = new Point(points[0].X, StartY);
+                    geometryContext.BeginFigure(sPoint, true, true);
+
+                    foreach (Point point in points)
+                        geometryContext.LineTo(point, true, true);
+                    var ePoint = new Point(points[points.Count - 1].X, StartY);
+                    geometryContext.LineTo(ePoint, true, true);
+                    geometryContext.Close();
+                }
+                drawingContext.DrawGeometry(rectBrush, null, streamGeometry);
+            }
+            DrawEllipse(rects, drawingContext);
         }
     }
 }
