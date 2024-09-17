8eae450062580df06c7c7788d2ce9f3e6586298e
闫驚鏵 <yanjinhuamail@163.com>
2024-09-17 17:55:38 +0800

    Create Theme
    Create Theme

---
+++ src/WPFDevelopers.Shared/Controls/Theme/ThemeModel.cs
@@ -0,0 +1,32 @@
+﻿using WPFDevelopers.Helpers;
+
+namespace WPFDevelopers.Controls
+{
+    public class ThemeModel : ViewModelBase
+    {
+        private bool _isChecked;
+
+        /// <summary>
+        ///     whether to choose
+        /// </summary>
+        public bool IsChecked
+        {
+            get => _isChecked;
+            set
+            {
+                _isChecked = value;
+                NotifyPropertyChange("IsChecked");
+            }
+        }
+
+        /// <summary>
+        ///     display color
+        /// </summary>
+        public string Color { get; set; }
+
+        /// <summary>
+        ///     resource path
+        /// </summary>
+        public string ResourcePath { get; set; }
+    }
+}
