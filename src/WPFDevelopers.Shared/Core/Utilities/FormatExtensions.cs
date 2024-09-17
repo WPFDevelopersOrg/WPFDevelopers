4be92a85ce3b54aa542289dd82b550f3631bab5f
闫驚鏵 <yanjinhuamail@163.com>
2024-09-17 17:57:59 +0800

    Create FormatExtensions.cs

---
+++ src/WPFDevelopers.Shared/Core/Utilities/FormatExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WPFDevelopers.Core
+{
+    public static class FormatExtensions
+    {
+        public static string FormatNumber(this object value)
+        {
+            if (value == null)
+                return string.Empty;
+            return Convert.ToDecimal(value).ToString("#,##0.#########################");
+        }
+    }
+
+}
