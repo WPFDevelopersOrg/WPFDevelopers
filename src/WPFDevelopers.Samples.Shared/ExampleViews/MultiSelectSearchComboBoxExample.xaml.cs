0f419d49011d26efdfa57bc3e6972e6525f61309
闫驚鏵 <yanjinhuamail@163.com>
2024-12-01 21:51:05 +0800

    Fix issue （#124 ）
    Fix issue （#124 ）

--- src/WPFDevelopers.Samples.Shared/ExampleViews/MultiSelectSearchComboBoxExample.xaml.cs
+++ src/WPFDevelopers.Samples.Shared/ExampleViews/MultiSelectSearchComboBoxExample.xaml.cs
@@ -1,5 +1,5 @@
-﻿using ICSharpCode.AvalonEdit.Document;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +11,24 @@ namespace WPFDevelopers.Samples.ExampleViews
     /// </summary>
     public partial class MultiSelectSearchComboBoxExample : UserControl
     {
+        public ObservableCollection<UserInfo> SelectedItems
+        {
+            get { return (ObservableCollection<UserInfo>)GetValue(SelectedItemsProperty); }
+            set { SetValue(SelectedItemsProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.Register("SelectedItems", typeof(ObservableCollection<UserInfo>), typeof(MultiSelectSearchComboBoxExample), new PropertyMetadata(null));
+
+        public ObservableCollection<UserInfo> ItemsSource
+        {
+            get { return (ObservableCollection<UserInfo>)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<UserInfo>), typeof(MultiSelectSearchComboBoxExample), new PropertyMetadata(null));
+
         public MultiSelectSearchComboBoxExample()
         {
             InitializeComponent();
@@ -19,11 +37,11 @@ namespace WPFDevelopers.Samples.ExampleViews
 
         private void MultiSelectSearchComboBoxExample_Loaded(object sender, RoutedEventArgs e)
         {
-            var list = new List<string>();
-            for (int i = 0; i < 10; i++)
-                list.Add(i.ToString());
-            MyMultiSelectionSearchComboBox2.ItemsSource = list;
-            MyMultiSelectionSearchComboBox2.SelectedItems = list.Skip(3).ToList();
+            //var list = new List<string>();
+            //for (int i = 0; i < 10; i++)
+            //    list.Add(i.ToString());
+            //MyMultiSelectionSearchComboBox2.ItemsSource = list;
+            //MyMultiSelectionSearchComboBox2.SelectedItems = list.Skip(3).ToList();
 
             var list2 = new List<UserInfo>();
             list2.Add(new UserInfo() { ID = "0", Name = "343DST.com" });
@@ -32,11 +50,19 @@ namespace WPFDevelopers.Samples.ExampleViews
             list2.Add(new UserInfo() { ID = "3", Name = "josh.peng" });
             MyMultiSelectionSearchComboBox3.ItemsSource = list2;
             MyMultiSelectionSearchComboBox3.SelectedItems = list2.Where(x => x.ID == "1" || x.ID == "3").ToList();
+
+            ItemsSource = new ObservableCollection<UserInfo>(list2);
+            SelectedItems = new ObservableCollection<UserInfo>(list2.Where(x => x.ID == "1" || x.ID == "3"));
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            WPFDevelopers.Controls.MessageBox.Show($"{MyMultiSelectionSearchComboBox2.Text} \r\n总共选中:{MyMultiSelectionSearchComboBox2.SelectedItems.Count} 条","选中内容",MessageBoxButton.OK,MessageBoxImage.Information);      
+            WPFDevelopers.Controls.MessageBox.Show($"{MyMultiSelectionSearchComboBox2.Text} \r\n总共选中:{SelectedItems.Count} 条", "选中内容", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void BtnAdd_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ItemsSource.Add(new UserInfo() { ID = "4", Name = "OpenAI.com" });
         }
     }
     public class UserInfo
