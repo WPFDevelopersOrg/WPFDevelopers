3fda4b8883587969262ffa9d3be965a1e391581d
闫驚鏵 <yanjinhuamail@163.com>
2024-12-17 17:19:40 +0800

    Fix issue （#125 ）
    Fix issue （#125 ）

--- src/WPFDevelopers.Samples.Shared/ExampleViews/MessageExample.xaml.cs
+++ src/WPFDevelopers.Samples.Shared/ExampleViews/MessageExample.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using WPFDevelopers.Controls;
 
@@ -12,6 +13,17 @@ namespace WPFDevelopers.Samples.ExampleViews
         public MessageExample()
         {
             InitializeComponent();
+           
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+            if (radioButton != null)
+            {
+                if (Enum.TryParse(radioButton.Content.ToString(), out Position position))
+                    Message.SetPosition(position);
+            }
         }
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
@@ -34,7 +46,38 @@ namespace WPFDevelopers.Samples.ExampleViews
                     Message.Push("这是一条很长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长消息", MessageBoxImage.Information);
                     break;
             }
+        }
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            Message.Clear();
+        }
+        private void BtnDesktopClear_Click(object sender, RoutedEventArgs e)
+        {
+            Message.ClearDesktop();
+        }
+        
 
+        private void AddButtonDesktop_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            switch (btn.Tag)
+            {
+                case "Info":
+                    Message.PushDesktop("This is a info message", MessageBoxImage.Information);
+                    break;
+                case "Error":
+                    Message.PushDesktop("This is a error message", MessageBoxImage.Error, true);
+                    break;
+                case "Warning":
+                    Message.PushDesktop("This is a warning message", MessageBoxImage.Warning, true);
+                    break;
+                case "Question":
+                    Message.PushDesktop("This is a question message", MessageBoxImage.Question);
+                    break;
+                default:
+                    Message.PushDesktop("这是一条很长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长消息", MessageBoxImage.Information);
+                    break;
+            }
         }
     }
 }
