836f874bc4165ee76689e12c0ec7ed8f9b8a4ff5
闫驚鏵 <yanjinhuamail@163.com>
2025-02-28 23:41:36 +0800

    Update MasterCarousel.cs

--- src/WPFDevelopers.Shared/Controls/Carousel/MasterCarousel.cs
+++ src/WPFDevelopers.Shared/Controls/Carousel/MasterCarousel.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Linq;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
