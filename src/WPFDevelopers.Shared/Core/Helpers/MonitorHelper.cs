6f781bc8cd1671263b053bdf5ec9099ce36772b0
闫驚鏵 <yanjinhuamail@163.com>
2025-03-12 21:42:55 +0800

    Update MonitorHelper.cs

--- src/WPFDevelopers.Shared/Core/Helpers/MonitorHelper.cs
+++ src/WPFDevelopers.Shared/Core/Helpers/MonitorHelper.cs
@@ -1,5 +1,4 @@
-﻿using Standard;
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 using WPFDevelopers.Helpers;
 
@@ -14,15 +13,6 @@ namespace WPFDevelopers.Core.Helpers
             public const int WM_SYSCOMMAND = 0x0112;
             public const int WM_DEADCHAR = 0x0024;
         }
-
-        [DllImport(Win32.User32)]
-        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [DllImport(Win32.User32)]
-        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
 
         [DllImport(Win32.User32)]
         internal static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
