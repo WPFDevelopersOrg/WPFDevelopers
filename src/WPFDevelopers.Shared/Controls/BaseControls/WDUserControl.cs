f825784cac2e21f3c32187a176ecf1c765c2b53e
闫驚鏵 <yanjinhuamail@163.com>
2025-08-06 23:07:54 +0800

    Fix issue : （#166 ）add WDUserControl

---
+++ src/WPFDevelopers.Shared/Controls/BaseControls/WDUserControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFDevelopers.Controls
+{
+    public class WDUserControl : ContentControl
+    {
+        static WDUserControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WDUserControl),
+                new FrameworkPropertyMetadata(typeof(WDUserControl)));
+        }
+    }
+}
