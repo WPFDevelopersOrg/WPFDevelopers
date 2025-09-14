9e38671f31dd4e73e4c0dc079d817a9655b1903a
闫驚鏵（Jinhua Yan） <yanjinhuamail@gmail.com>
2025-09-14 22:15:48 +0800

    Create FalseToVisibilityConverter.cs

---
+++ src/WPFDevelopers.Shared/Core/Converts/FalseToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPFDevelopers.Converts
+{
+    public class FalseToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
