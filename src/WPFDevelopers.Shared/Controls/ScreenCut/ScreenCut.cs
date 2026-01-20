bf51f655e81a3882a6e5f3d3d0b53082221a231c
闫驚鏵（Jinhua Yan） <yanjinhuamail@gmail.com>
2026-01-20 23:47:53 +0800

    Update ScreenCut.cs

--- src/WPFDevelopers.Shared/Controls/ScreenCut/ScreenCut.cs
+++ src/WPFDevelopers.Shared/Controls/ScreenCut/ScreenCut.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -14,7 +12,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 using WPFDevelopers.Helpers;
 using Brush = System.Windows.Media.Brush;
 using Brushes = System.Windows.Media.Brushes;
