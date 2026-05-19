99ff1ed55b61ae9df8d27fc57967ebfdc9d15b57
闫驚鏵（Jinhua Yan） <yanjinhuamail@gmail.com>
2026-05-19 23:44:21 +0800

    Fix issue:  DataGrid FilterEngine is not empty display filter icon （#247） 💯

---
+++ src/WPFDevelopers.Shared/Core/Converts/FilterVisibilityMultiConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WPFDevelopers.Converts
+{
+    public class FilterVisibilityMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2) return Visibility.Collapsed;
+
+            var column = values[0] as DataGridColumn;
+            if (column == null) return Visibility.Collapsed;
+            var filterEngine = values[1];
+            bool isValidColumn = !string.IsNullOrEmpty(column.Header?.ToString())
+                                 || column is DataGridBoundColumn
+                                 || (column is DataGridTemplateColumn templateColumn && templateColumn.CellTemplate != null);
+
+            bool hasFilterEngine = filterEngine != null;
+            if (isValidColumn && hasFilterEngine)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
