1821cedfcba3f9dfb8123bdb137112569a7375f5
闫驚鏵（Jinhua Yan） <yanjinhuamail@gmail.com>
2026-07-08 22:48:08 +0800

    feat: modify MainWindow add CircleMenu

--- src/WPFDevelopers.Samples.Shared/ExampleViews/MainWindow.xaml.cs
+++ src/WPFDevelopers.Samples.Shared/ExampleViews/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media.Animation;
 using WPFDevelopers.Controls;
 using WPFDevelopers.Samples.ExampleViews;
+using WPFDevelopers.Samples.Helpers;
 using MessageBox = WPFDevelopers.Controls.MessageBox;
 
 namespace WPFDevelopers.Samples
@@ -58,17 +61,17 @@ namespace WPFDevelopers.Samples
             if(grayscaleEffect.Factor == 1)
             {
                 menuItemGrayscale.Header = "关闭灰度";
-                Create(0);
+                CreateGrayscale(0);
             }
                
             else
             {
                 menuItemGrayscale.Header = "开启灰度";
-                Create(1);
+                CreateGrayscale(1);
 
             }
         }
-        void Create(double to)
+        void CreateGrayscale(double to)
         {
             var sineEase = new SineEase() { EasingMode = EasingMode.EaseOut };
             var doubleAnimation = new DoubleAnimation
@@ -91,6 +94,77 @@ namespace WPFDevelopers.Samples
             WpfNotifyIcon.IsTwink = !WpfNotifyIcon.IsTwink;
             menuItemTwink.IsChecked = WpfNotifyIcon.IsTwink;
         }
+
+        public ICommand CircleMenuItemClickCommand => new RelayCommand(param =>
+        {
+            var item = param as CircleMenuItem;
+            if (item == null) return;
+            var name = item.Name.ToString();
+            if (string.IsNullOrWhiteSpace(name)) return;
+            switch (name)
+            {
+                case "ScreenShot":
+                    Application.Current.MainWindow.WindowState = WindowState.Minimized;
+                    var screenCaptureExt = new ScreenCaptureExt();
+                    screenCaptureExt.SnapCanceled += ScreenCaptureExt_SnapCanceled;
+                    screenCaptureExt.SnapCompleted += ScreenCaptureExt_SnapCompleted;
+                    break;
+                case "Github":
+                    var uri = "https://github.com/WPFDevelopersOrg/WPFDevelopers";
+                    if (uri.StartsWith("http://") || uri.StartsWith("https://"))
+                    {
+                        Process.Start(new ProcessStartInfo(uri) { UseShellExecute = true });
+                    }
+                    break;
+                case "Grayscale":
+                    var isEnable = GrayscaleSvg.Source.Contains("Enable");
+                    GrayscaleSvg.Source = isEnable
+               ? "pack://application:,,,/WPFDevelopers.Samples;component/Resources/Svg/DisableGrayscale.svg"
+               : "pack://application:,,,/WPFDevelopers.Samples;component/Resources/Svg/EnableGrayscale.svg";
+                    if(isEnable)
+                        CreateGrayscale(0);
+                    else
+                        CreateGrayscale(1);
+                    break;
+                case "Theme":
+                    var isDark = ThemeSvg.Source.Contains("Moon");
+                    var theme = isDark ? ThemeType.Dark : ThemeType.Light;
+                    if (App.Theme == theme) return;
+                    App.Theme = theme;
+                    ThemeManager.Instance.SetTheme(theme);
+                    SetThemeSource();
+                    break;
+                case "Code":
+                    CodeDrawer.IsOpen = !CodeDrawer.IsOpen;
+                    break;
+                case "ClearToast":
+                    Toast.ClearAll();
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        void SetThemeSource()
+        {
+            App.Theme = ThemeManager.Instance.Resources.Theme;
+            if (App.Theme == ThemeType.Dark)
+                ThemeSvg.Source = "pack://application:,,,/WPFDevelopers.Samples;component/Resources/Svg/Sun.svg";
+            else
+                ThemeSvg.Source = "pack://application:,,,/WPFDevelopers.Samples;component/Resources/Svg/Moon.svg";
+        }
+
+        private void ScreenCaptureExt_SnapCanceled()
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
+        private void ScreenCaptureExt_SnapCompleted(System.Windows.Media.Imaging.BitmapSource bitmap)
+        {
+
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
         private void ColorPicker_Apply(object sender, RoutedEventArgs e)
         {
             ThemeSwitchBtn.IsChecked = false;
